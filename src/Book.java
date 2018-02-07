05ba1a677793744d7e7df83a7a98e6509b554308
KassondraJones <kassondralj@yahoo.com>
2018-02-07 15:53:04 -0500

    updated to current status

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 public class Book {
@@ -65,4 +75,63 @@ public class Book {
 				genre, status, dueDate);
 	}
 
+
+	public static void readFromFile() {
+		Path writeFile = Paths.get("terminal.txt");
+		File file = writeFile.toFile();
+
+		try {
+			FileReader fr = new FileReader(file);
+
+			BufferedReader reader = new BufferedReader(fr);
+
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("There were no books!");
+		}
+
+	}
+	
+	
+	
+	public static void writeToFile(String name) {
+		Path writeFile = Paths.get("terminal.txt");
+		File file = writeFile.toFile();
+
+		try {
+			PrintWriter out = new PrintWriter(new FileOutputStream(file, true));
+			out.println( name);
+			out.close();
+		} catch (FileNotFoundException e) {
+			createFile("terminal.txt");
+			try {
+				PrintWriter out = new PrintWriter(new FileOutputStream(file, true));
+				out.println( name);
+				out.close();
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public static void createFile(String fileString) {
+		Path filePath = Paths.get(fileString);
+		if (Files.notExists(filePath)) {
+			try {
+				Files.createFile(filePath);
+				System.out.println("Your file was cerated successfully");
+			} catch (IOException e) {
+				System.out.println("Something went wrong with file creation ");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
 }
--- src/LibraryTerminalApp.java
+++ src/LibraryTerminalApp.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -8,59 +9,75 @@ public class LibraryTerminalApp {
 		Scanner scan = new Scanner(System.in);
 		int choice = 2;
 
+
 		System.out.println("Welcome to the Grand Circus Library Terminal!");
 		
-		LinkedList<String> books = new LinkedList<String>();
-		books.add("To Kill a Mockingbird\t\tHarper Lee");
-		books.add("Pride & Prejuidice\t\tJane Austen");
-		books.add("Animal Farm\t\t\tGeorge Orwell");
-		books.add("The Book Theif\t\t\tMarkus Zusak");
-		books.add("Lord of the Flies\t\tWilliam Golding");
-		books.add("Wuthering Heights\t\tEmily Bronte");
-		books.add("A Brief History of Time\t\tStephen Hawking");
-		books.add("No Logo\t\t\t\tNaomi Klein");
-		books.add("Dispatches\t\t\tMichael Herr");
-		books.add("Hiroshima\t\t\tJohn Hersey");
-		books.add("The Columbia Encyclopedia\tPaul Legasse");
-		books.add("Roget's Thesaurus\t\tPeter Roget");
-					
-			
+
 		while (choice != 6) {
 
 			System.out.println();
 			choice = Validator.getInt(scan,
 					" 1 - See Catalog of Books\n 2 - Search Books by Author\n 3 - Search Books by Title Keyword\n 4 - Select a Book to Check-out\n 5 - Return a Book\n 6 - Quit",
 					1, 6);
+			
 			if (choice == 1) {
-				for (int i = 0; i < books.size(); i++) {
-					System.out.println(books.get(i) + "\t\t" + BookStatus.AVAILABLE);
-				}
-		//		CountriesTextFile.readFromFile();
-			} else if (choice == 2) {
+				Book.readFromFile();
+			}
+			if (choice == 2) {
 				System.out.println();
 				String authorName = Validator.getString(scan, "Enter the author name: ");
-			} else if (choice == 3) {
+				
+			}
+			
+			if (choice == 3) {
 				System.out.println();
 				String titleKeyword = Validator.getString(scan, "Enter the title keyword: ");
-			} else if (choice == 4) {
-				System.out.println();
-				for (int i = 0; i < books.size(); i++) {
-					System.out.println(i + "\t" + books.get(i) + "\t\t" + BookStatus.AVAILABLE);
-				}
+				
+			}
+			
+			if (choice == 4) {
 				System.out.println();
 				String bookTitleCO = Validator.getString(scan, "Which book would you like to check out: ");
-			} else if (choice == 5) {
+			}
+			if (choice == 5) {
 				System.out.println();
 				String bookTitleRT = Validator.getString(scan, "Which book would you like to return: ");
-			} else if (choice == 6){
+				
+				
+				
+			}
+			if (choice == 6){
 				System.out.println();
 				System.out.println("Thank you for visiting to Grand Circus Library Terminal!\nGoodbye!");
-				
-			
 			}
 
 		}
 
-	}
 
+		
+	}
+	
+	
+	
+	
+	
+/*	public static searchList {
+		
+		LinkedList<Book> books = new LinkedList<Book>();
+	        books.add("To Kill a Mockingbird", "Harper Lee");
+	        books.add("Pride & Prejuidice", "Jane Austen");
+	        books.add("Animal Farm", "George Orwell");
+	        books.add("The Book Theif", "Markus Zusak");
+	        books.add("Lord of the Flies", "William Golding");
+	        books.add("Wuthering Heights", "Emily Bronte");
+	        books.add("A Brief History of Time", "Stephen Hawking");
+	        books.add("No Logo", "Naomi Klein");
+	        books.add("Dispatches", "Michael Herr");
+	        books.add("Hiroshima", "John Hersey");
+	        books.add("The Columbia Encyclopedia", "Paul Legasse");
+	        books.add("Roget's Thesaurus", "Peter Roget");
+		
+	}*/
 }
+
+
