da677751d545397a69f9c48322a8411ac415cece
Bjorn74 <benfogt@gmail.com>
2018-02-08 14:14:55 -0500

    Getting ready to remove TableGUI.java

--- src/gui/TableGUI.java
+++ src/gui/TableGUI.java
@@ -1,28 +1,68 @@
 package gui;
-import javax.swing.table.AbstractTableModel;
+//package net.codejava.swing;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
 
-public class TableGUI extends AbstractTableModel {
-
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class TableGUI extends JFrame
+{
+    
 	
-	// Search Methods
-	
-	
-	
+	/*
+	 * public void TableBuild()
+	 
+    {
+        //headers for the table
+        String[] columns = new String[] {
+            "Title", "Author", "Status", "Due Date", "Genre"
+        };
+        
+        Book[] books = new Book[]()
+        //actual data for the table in a 2d array
+        //Object[] data = new Book[] {
+        //  {1, "John", 40.0, false },
+        //    {2, "Rambo", 70.0, false },
+        //    {3, "Zorro", 60.0, true },
+        //};
+        
+        final Class[] columnClass = new Class[] {
+            Integer.class, String.class, Double.class, Boolean.class
+        };
+        //create table model with data
+        DefaultTableModel model = new DefaultTableModel(data, columns) {
+            @Override
+            public boolean isCellEditable(int row, int column)
+            {
+                return false;
+            }
+            @Override
+            public Class<?> getColumnClass(int columnIndex)
+            {
+                return columnClass[columnIndex];
+            }
+        };
+        
+        JTable table = new JTable(model);
+        
+        //add the table to the frame
+        this.add(new JScrollPane(table));
+        
+        this.setTitle("Table Example");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        this.pack();
+        this.setVisible(true);
+    }
+    
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TableExample();
+            }
+        });
+    }  
+    */
 }
