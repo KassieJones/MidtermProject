970c0e14db4ca0b9f7ae2e11f92dbbd792aca50a
Bjorn74 <benfogt@gmail.com>
2018-02-08 18:54:32 -0500

    Got the Table mostly working

--- src/gui/LibraryTerminalGUI.java
+++ src/gui/LibraryTerminalGUI.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.util.ArrayList;
@@ -12,11 +13,12 @@ import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
 import model.Book;
+import javax.swing.JLabel;
 
 public class LibraryTerminalGUI {
 
 	private JFrame frame;
-	private JTable table;
+	private JTable tableView;
 
 	/**
 	 * Launch the application.
@@ -30,8 +32,7 @@ public class LibraryTerminalGUI {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
-				
+
 			}
 		});
 	}
@@ -50,12 +51,12 @@ public class LibraryTerminalGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 517, 418);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		JMenu menu, submenu;
 		JMenuItem menuItem;
-		
+
 		// Build Menus
 		JMenu menuExit = new JMenu("Exit");
 		JMenu menuCheckOut = new JMenu("CheckOut");
@@ -66,7 +67,7 @@ public class LibraryTerminalGUI {
 		JMenu menuAdd = new JMenu("Add Book(s)");
 		JMenuItem menuAddOne = new JMenuItem("Add One");
 		JMenuItem menuAddFile = new JMenuItem("Add from File");
-		
+
 		menuBar.add(menuExit);
 		menuBar.add(menuCheckOut);
 		menuBar.add(menuReturn);
@@ -77,18 +78,57 @@ public class LibraryTerminalGUI {
 		menuAdd.add(menuAddOne);
 		menuAdd.add(menuAddFile);
 		
-		
-		
-		ArrayList<Book> library = new ArrayList<Book>();
-		table = new JTable();
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		frame.getContentPane().add(table, BorderLayout.CENTER);
-		
-		DefaultTableModel model = new DefaultTableModel();
-		
-		
-		
-		
+		tableView = new JTable();
+		tableView.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		frame.getContentPane().add(tableView, BorderLayout.SOUTH);
+
+	
+		ArrayList<Book> library = Book.readFromFile("Library.json");
+		//frame.getContentPane().add(displayTable, BorderLayout.CENTER);
+
+		/*
+		  DefaultTableModel model = new DefaultTableModel();
+		 
+	
+
+		//String[] columnTitles = { "Title", "Author", "Status", "Due Date", "Genre" };
+		//model.setColumnIdentifiers(columnTitles);
+		model.addColumn("Title");
+		model.addColumn("Author");
+		model.addColumn("Status");
+		model.addColumn("Due Date");
+		model.addColumn("Genre");
+		for (Book book : library) {
+			String[] string = { book.getTitle(), book.getAuthor(), book.getStatus(), book.getDueDate(),
+					book.getGenre() };
+			model.addRow(string);
+		}
+		//lblNewLabel.setText(model.toString());
+		//displayTable.setModel(model);
+		//displayTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		//frame.getContentPane().add(displayTable, BorderLayout.CENTER);
+*/
+	
+		String[] columns = new String[] {
+	            "Title", "Author", "Status", "Due Date", "Genre"
+	        };
+	         
+	        //actual data for the table in a 2d array
+	        String[][] data = new String[library.size()][5];
+	        for (int i = 0; i < library.size(); i++) {
+	        	data[i][0] = library.get(i).getTitle();
+	        	data[i][1] = library.get(i).getAuthor();
+	        	data[i][2] = library.get(i).getStatus();
+	        	data[i][3] = library.get(i).getDueDate();
+	        	data[i][4] = library.get(i).getGenre();
+	        }
+	        	//create table with data
+	        tableView = new JTable(data, columns);
+	        frame.add(tableView);
+	        
+	         
+
+	
 	}
 
 }
