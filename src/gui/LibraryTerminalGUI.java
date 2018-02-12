bd6f5682f7b85a2bef87789ea8519e93ffa79a71
Bjorn74 <benfogt@gmail.com>
2018-02-11 22:59:26 -0500

    Rewrote GUI

--- src/gui/LibraryTerminalGUI.java
+++ src/gui/LibraryTerminalGUI.java
@@ -1,24 +1,32 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import model.Book;
-import javax.swing.JLabel;
 
 public class LibraryTerminalGUI {
 
 	private JFrame frame;
-	private JTable tableView;
+	int aListPos = 0;
 
 	/**
 	 * Launch the application.
@@ -51,84 +59,170 @@ public class LibraryTerminalGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 517, 418);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
+		
+		// Make the all important arraylist
+		ArrayList<Book> library = Book.readFromFile("Library.json");
+		String[] bookList = new String[library.size()];
+		for (int i = 0; i < library.size(); i++) {
+			bookList[i] = library.get(i).getTitle();
+		}
+
+		// JSplitPane gives us a divided window. The left displays book titles.
+		// Clicking one brings the book's information to the left pane.
+		// Menu bar actions will permit checkout and other activities.
+
+		JSplitPane splitPane = new JSplitPane();
+		frame.getContentPane().add(splitPane);
+
+		JList list = new JList(bookList);
+		splitPane.setLeftComponent(list);
+
+		JPanel panel = new JPanel();
+		splitPane.setRightComponent(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0, 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		JLabel lblTitle = new JLabel("Title");
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitle.gridx = 1;
+		gbc_lblTitle.gridy = 1;
+		panel.add(lblTitle, gbc_lblTitle);
+
+		JLabel titleLabel = new JLabel("Title");
+		GridBagConstraints gbc_titleLabel = new GridBagConstraints();
+		gbc_titleLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_titleLabel.gridx = 3;
+		gbc_titleLabel.gridy = 1;
+		panel.add(titleLabel, gbc_titleLabel);
+
+		JLabel lblAuthor = new JLabel("Author");
+		GridBagConstraints gbc_lblAuthor = new GridBagConstraints();
+		gbc_lblAuthor.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAuthor.gridx = 1;
+		gbc_lblAuthor.gridy = 3;
+		panel.add(lblAuthor, gbc_lblAuthor);
+
+		JLabel authorLabel = new JLabel("Author");
+		GridBagConstraints gbc_authorLabel = new GridBagConstraints();
+		gbc_authorLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_authorLabel.gridx = 3;
+		gbc_authorLabel.gridy = 3;
+		panel.add(authorLabel, gbc_authorLabel);
+
+		JLabel lblStatus = new JLabel("Status");
+		GridBagConstraints gbc_lblStatus = new GridBagConstraints();
+		gbc_lblStatus.insets = new Insets(0, 0, 5, 5);
+		gbc_lblStatus.gridx = 1;
+		gbc_lblStatus.gridy = 5;
+		panel.add(lblStatus, gbc_lblStatus);
+
+		JLabel statusLabel = new JLabel("Status");
+		GridBagConstraints gbc_statusLabel = new GridBagConstraints();
+		gbc_statusLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_statusLabel.gridx = 3;
+		gbc_statusLabel.gridy = 5;
+		panel.add(statusLabel, gbc_statusLabel);
+
+		JLabel lblDueDate = new JLabel("Due Date");
+		GridBagConstraints gbc_lblDueDate = new GridBagConstraints();
+		gbc_lblDueDate.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDueDate.gridx = 1;
+		gbc_lblDueDate.gridy = 7;
+		panel.add(lblDueDate, gbc_lblDueDate);
+
+		JLabel dueDateLabel = new JLabel("Due Date");
+		GridBagConstraints gbc_dueDateLabel = new GridBagConstraints();
+		gbc_dueDateLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_dueDateLabel.gridx = 3;
+		gbc_dueDateLabel.gridy = 7;
+		panel.add(dueDateLabel, gbc_dueDateLabel);
+
+		JLabel lblGenre = new JLabel("Genre");
+		GridBagConstraints gbc_lblGenre = new GridBagConstraints();
+		gbc_lblGenre.insets = new Insets(0, 0, 0, 5);
+		gbc_lblGenre.gridx = 1;
+		gbc_lblGenre.gridy = 9;
+		panel.add(lblGenre, gbc_lblGenre);
+
+		JLabel genreLabel = new JLabel("Genre");
+		GridBagConstraints gbc_genreLabel = new GridBagConstraints();
+		gbc_genreLabel.gridx = 3;
+		gbc_genreLabel.gridy = 9;
+		panel.add(genreLabel, gbc_genreLabel);
+
+		// Listener changes the display on the left when a book is selected on the
+		// right.
+
+		list.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				if (!arg0.getValueIsAdjusting()) {
+					// titleLabel.setText(list.getSelectedValue().toString());
+					// System.out.println(list.getSelectedValue().toString());
+					//
+					//int aListPos = 0;
+					for (int i = 0; i < library.size(); i++)
+						if (library.get(i).getTitle().contains((String) list.getSelectedValue())) {
+							aListPos = i;
+							i = (library.size());
+
+						}
+					titleLabel.setText(library.get(aListPos).getTitle());
+					authorLabel.setText(library.get(aListPos).getAuthor());
+					statusLabel.setText(library.get(aListPos).getStatus());
+					dueDateLabel.setText(library.get(aListPos).getDueDate());
+					genreLabel.setText(library.get(aListPos).getGenre());
+
+				}
+			}
+		});
 
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		JMenu menu, submenu;
-		JMenuItem menuItem;
 
-		// Build Menus
-		JMenu menuExit = new JMenu("Exit");
-		JMenu menuCheckOut = new JMenu("CheckOut");
-		JMenu menuReturn = new JMenu("Return");
-		JMenuItem menuSearchKW = new JMenuItem("By Title Keyword");
-		JMenuItem menuSearchAuthor = new JMenuItem("By Author");
-		JMenu menuSearch = new JMenu("Search");
-		JMenu menuAdd = new JMenu("Add Book(s)");
-		JMenuItem menuAddOne = new JMenuItem("Add One");
-		JMenuItem menuAddFile = new JMenuItem("Add from File");
+		JMenuItem mntmCheckOut = new JMenuItem("Check Out");
+		menuBar.add(mntmCheckOut);
+		mntmCheckOut.addActionListener(new ActionListener() {
 
-		menuBar.add(menuExit);
-		menuBar.add(menuCheckOut);
-		menuBar.add(menuReturn);
-		menuBar.add(menuSearch);
-		menuSearch.add(menuSearchAuthor);
-		menuSearch.add(menuSearchKW);
-		menuBar.add(menuAdd);
-		menuAdd.add(menuAddOne);
-		menuAdd.add(menuAddFile);
-		
-		tableView = new JTable();
-		tableView.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		frame.getContentPane().add(tableView, BorderLayout.SOUTH);
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				library.get(aListPos).setStatus("UNAVAILABLE");
+				library.get(aListPos).setDueDate(LocalDate.now().plusWeeks(2).toString());
+				statusLabel.setText(library.get(aListPos).getStatus());
+				dueDateLabel.setText(library.get(aListPos).getDueDate());
+			}
 
+		});
+
+		JMenuItem mntmReturn = new JMenuItem("Return");
+		menuBar.add(mntmReturn);
+		mntmReturn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				library.get(aListPos).setStatus("AVAILABLE");
+				library.get(aListPos).setDueDate(" ");
+				statusLabel.setText(library.get(aListPos).getStatus());
+				dueDateLabel.setText(library.get(aListPos).getDueDate());
+			}
+
+		});
 	
-		ArrayList<Book> library = Book.readFromFile("Library.json");
-		//frame.getContentPane().add(displayTable, BorderLayout.CENTER);
-
-		/*
-		  DefaultTableModel model = new DefaultTableModel();
-		 
-	
-
-		//String[] columnTitles = { "Title", "Author", "Status", "Due Date", "Genre" };
-		//model.setColumnIdentifiers(columnTitles);
-		model.addColumn("Title");
-		model.addColumn("Author");
-		model.addColumn("Status");
-		model.addColumn("Due Date");
-		model.addColumn("Genre");
-		for (Book book : library) {
-			String[] string = { book.getTitle(), book.getAuthor(), book.getStatus(), book.getDueDate(),
-					book.getGenre() };
-			model.addRow(string);
-		}
-		//lblNewLabel.setText(model.toString());
-		//displayTable.setModel(model);
-		//displayTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//frame.getContentPane().add(displayTable, BorderLayout.CENTER);
-*/
-	
-		String[] columns = new String[] {
-	            "Title", "Author", "Status", "Due Date", "Genre"
-	        };
-	         
-	        //actual data for the table in a 2d array
-	        String[][] data = new String[library.size()][5];
-	        for (int i = 0; i < library.size(); i++) {
-	        	data[i][0] = library.get(i).getTitle();
-	        	data[i][1] = library.get(i).getAuthor();
-	        	data[i][2] = library.get(i).getStatus();
-	        	data[i][3] = library.get(i).getDueDate();
-	        	data[i][4] = library.get(i).getGenre();
-	        }
-	        	//create table with data
-	        tableView = new JTable(data, columns);
-	        frame.add(tableView);
-	        
-	         
-
-	
+		// Exit Listener
+				frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			        public void windowClosing(WindowEvent winEvt) {
+			            Book.writeToFile(library);
+			            System.exit(0);
+			        }
+			    });
 	}
 
+	
 }
